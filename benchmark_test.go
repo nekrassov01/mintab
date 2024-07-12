0759b0f7ac79a6ddd721cc12c9de912211cdf2cd
nekrassov01 <unclek62@gmail.com>
2024-07-13 02:41:42 +0900

    test: add benchmark test

---
+++ benchmark_test.go
@@ -0,0 +1,63 @@
+package mintab
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/olekukonko/tablewriter"
+)
+
+func BenchmarkMintab(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		table := New(&bytes.Buffer{})
+		if err := table.Load(basicsample); err != nil {
+			b.Fatal(err)
+		}
+		table.Out()
+	}
+}
+
+func BenchmarkMintabSimple(b *testing.B) {
+	data := []struct {
+		InstanceID    string
+		InstanecName  string
+		InstanceState string
+	}{
+		{InstanceID: "i-1", InstanecName: "server-1", InstanceState: "running"},
+		{InstanceID: "i-2", InstanecName: "server-2", InstanceState: "stopped"},
+		{InstanceID: "i-3", InstanecName: "server-3", InstanceState: "pending"},
+		{InstanceID: "i-4", InstanecName: "server-4", InstanceState: "terminated"},
+		{InstanceID: "i-5", InstanecName: "server-5", InstanceState: "stopping"},
+		{InstanceID: "i-6", InstanecName: "server-6", InstanceState: "shutting-down"},
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		table := New(&bytes.Buffer{})
+		if err := table.Load(data); err != nil {
+			b.Fatal(err)
+		}
+		table.Out()
+	}
+}
+
+func BenchmarkTableWriter(b *testing.B) {
+	header := []string{"InstanceID", "InstanceName", "InstanceState"}
+	data := [][]string{
+		{"i-1", "server-1", "running"},
+		{"i-2", "server-2", "stopped"},
+		{"i-3", "server-3", "pending"},
+		{"i-4", "server-4", "terminated"},
+		{"i-5", "server-5", "stopping"},
+		{"i-6", "server-6", "shutting-down"},
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		table := tablewriter.NewWriter(&bytes.Buffer{})
+		table.SetHeader(header)
+		for _, v := range data {
+			table.Append(v)
+		}
+		table.Render()
+	}
+}
--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/nekrassov01/mintab
 
 go 1.21
 
-require github.com/mattn/go-runewidth v0.0.15
+require (
+	github.com/mattn/go-runewidth v0.0.15
+	github.com/olekukonko/tablewriter v0.0.5
+)
 
 require github.com/rivo/uniseg v0.2.0 // indirect
